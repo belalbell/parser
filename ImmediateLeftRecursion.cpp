1e2ceda0a56002a50705f68478d928e0c410a227
saraEhab <sara3wahdan@gmail.com>
2019-04-22 01:27:53 +0200

    isImmediateLeftRecursionMethod is completed

--- ImmediateLeftRecursion.cpp
+++ ImmediateLeftRecursion.cpp
@@ -3,5 +3,14 @@
 //
 
 #include "ImmediateLeftRecursion.h"
+
 void ImmediateLeftRecursion::eliminateImmediateLeftRecursion(string symbol, vector <string> splittedGrammar) {}
-bool ImmediateLeftRecursion::isImmediateLeftRecursion(string symbol, vector <string> splittedGrammar) {}
+
+bool ImmediateLeftRecursion::isImmediateLeftRecursion(string symbol, vector <string> splittedGrammar) {
+    for (int nodeIndex = 0; nodeIndex < splittedGrammar.size(); ++nodeIndex) {
+        if (splittedGrammar[nodeIndex].find(symbol) != string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
