2072cb9e5ce7c06e7f26fcfd11fee6863d3b59a0
bolal <https://github.com/belalbell>
2019-04-22 23:51:07 +0200

    func to help on output

--- parsing.h
+++ parsing.h
@@ -7,28 +7,30 @@
 
 #include <string>
 #include <map>
-
+#include "InputHandler.h"
 
 using namespace std;
 
 class parsing {
 private:
-    string** parsingTable =new string*();
+    InputHandler* inputHandler = InputHandler::getInstance();
     map<string,int> terminalIndex;
     map<string,int> lhsIndex;
+    map<string,vector<string>> firstMap;
+    map<string,vector<string>> followMap;
+    vector<vector<string>> parsingTable;
 
 public:
 
     static parsing* getInstance();
     string** detParsingTable();
-    int getColSize();
-    int getRowSize();
     void first();
     void follow();
     int getTerminalIndex(string terminal);
     int getLhsIndex(string lhs);
     map<string,int> getterminalIndex();
     map<string,int> getlhsIndex();
+    vector<vector<string>> getParsingTable();
 
 };
 
