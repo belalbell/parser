41b4ce873e985680743cc356fb05ecf910273673
saraEhab <sara3wahdan@gmail.com>
2019-04-22 03:15:04 +0200

    elimminateImmediateLeftRecursion method is completed

--- ImmediateLeftRecursion.cpp
+++ ImmediateLeftRecursion.cpp
@@ -17,7 +17,21 @@ void ImmediateLeftRecursion::eliminateImmediateLeftRecursion(string symbol, vect
     vector<string> firstGrammar, secondGrammar;
     string temp = "", symbolDash = symbol + "'";
     int symbolSize = symbol.size();
-
+    inputHandler->setLHS(symbolDash);
+    for (int nodeIndex = 0; nodeIndex < splittedGrammar.size(); ++nodeIndex) {
+        /*check if the current left hand side is the most left symbol in the right hand side  */
+        if (splittedGrammar[nodeIndex].find(symbol) == 0) {
+            temp = splittedGrammar[nodeIndex].substr(symbolSize, splittedGrammar[nodeIndex].size() - 1);
+            temp += " " + symbolDash;
+            secondGrammar.push_back(temp);
+        } else if (splittedGrammar[nodeIndex].find(symbol) == string::npos) {
+            temp = splittedGrammar[nodeIndex] + " " + symbolDash;
+            firstGrammar.push_back(temp);
+        }
+    }
+    secondGrammar.push_back("\\L");
+    inputHandler->setLeftToRight(symbol, firstGrammar);
+    inputHandler->setLeftToRight(symbolDash, secondGrammar);
 }
 
 bool ImmediateLeftRecursion::isImmediateLeftRecursion(string symbol, vector<string> splittedGrammar) {
