906863a904cbf0259d573c2fe278b4cdad97b74f
bolal <https://github.com/belalbell>
2019-04-22 22:40:33 +0200

    first rhs

--- parsing.cpp
+++ parsing.cpp
@@ -28,10 +28,10 @@ void parsing::first() {
             vector<string> token = inputHandler->getRightToTokens(rhs.at(j));
             if (!(token.at(0)[0] >= 'A' && token.at(0)[0] <= 'Z')) {
                 tmp->push_back(token.at(0));
-            } else{
+            } else {
                 string tmpStr = token.at(0);
                 tmpStr.erase(remove_if(tmpStr.begin(), tmpStr.end(), ::isspace), tmpStr.end());
-                if (lhs!=tmpStr) {
+                if (lhs != tmpStr) {
                     vector<string> nonTerminals = firstMap[tmpStr];
                     for (int k = 0; k < nonTerminals.size(); ++k) {
                         tmp->push_back(nonTerminals.at(k));
@@ -39,8 +39,30 @@ void parsing::first() {
                 }
             }
         }
-        firstMap[lhs]=*tmp;
+        firstMap[lhs] = *tmp;
     }
-    cout<<"first lhs done ";
+    cout << "first lhs done ";
+    for (int l = 0; l < lhsSize; ++l) {
+        vector<string> rhs = inputHandler->getRHSByIndex(l);
+        for (int i = 0; i < rhs.size(); ++i) {
+            vector<string> *tmp = new vector<string>();
+            string rhsElement = rhs.at(i);
+            //rhsElement.erase(remove_if(rhsElement.begin(), rhsElement.end(), ::isspace), rhsElement.end());
+            vector<string> token = inputHandler->getRightToTokens(rhs.at(i));
+            if (!(token.at(0)[0] >= 'A' && token.at(0)[0] <= 'Z')) {
+                tmp->push_back(token.at(0));
+                firstMap[rhsElement]=*tmp;
+            } else{
+                vector<string> takeFirst = firstMap[token.at(0)];
+                for (int j = 0; j < takeFirst.size(); ++j) {
+                    tmp->push_back(takeFirst.at(j));
+                }
+            }
+            firstMap[rhsElement]=*tmp;
+        }
+    }
+    cout << "first lhs done ";
 }
+
+
 
