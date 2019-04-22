1f0c7d05b2d34e14b8d14f971bfd2cdcb43d2e5d
saraEhab <sara3wahdan@gmail.com>
2019-04-22 09:08:05 +0200

    getASymbolName function is completed

--- SymbolsNames.cpp
+++ SymbolsNames.cpp
@@ -13,4 +13,13 @@ SymbolsNames *SymbolsNames::getInstance() {
     return instance;
 }
 
-string SymbolsNames::getASymbolName(string originalSymbol) {}
+string SymbolsNames::getASymbolName(string originalSymbol) {
+    string newSymbol = originalSymbol + "'";
+    vector<string> lhs = inputHandler->getLHS();
+    for (int lhsIndex = 0; lhsIndex < lhs.size(); ++lhsIndex) {
+        if (newSymbol == lhs[lhsIndex]) {
+            newSymbol += "'";
+        }
+    }
+    return newSymbol;
+}
