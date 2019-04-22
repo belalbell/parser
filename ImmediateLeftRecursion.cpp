7e2344f665a47b5d25db5753ba77a137fa275888
saraEhab <sara3wahdan@gmail.com>
2019-04-22 17:15:30 +0200

    fixing isImmediateLeftRecursion method

--- ImmediateLeftRecursion.cpp
+++ ImmediateLeftRecursion.cpp
@@ -36,7 +36,7 @@ void ImmediateLeftRecursion::eliminateImmediateLeftRecursion(string symbol, vect
 
 bool ImmediateLeftRecursion::isImmediateLeftRecursion(string symbol, vector<string> splittedGrammar) {
     for (int nodeIndex = 0; nodeIndex < splittedGrammar.size(); ++nodeIndex) {
-        if (splittedGrammar[nodeIndex].find(symbol) != string::npos) {
+        if (splittedGrammar[nodeIndex].find(symbol) == 0) {
             return true;
         }
     }
